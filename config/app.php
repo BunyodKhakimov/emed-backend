bd31db32b36276d25a6451847f5d202ebbc12dd8
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-03-06 14:55:08 +0500

    add tymon/jwt-auth provider

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Nexmo\Laravel\NexmoServiceProvider::class
+        Nexmo\Laravel\NexmoServiceProvider::class,
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class
     ],
 
     /*
