3e0e63e7d75cecc7a6bb8bf5a48d65e76b8d13b7
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-03-06 19:03:55 +0500

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -14,21 +14,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::get('/register', 'AuthController@refister')->name('custom.register');
+Route::get('auth/register', 'AuthController@register')->name('custom.register');
+Route::get('auth/verify', 'AuthController@verify')->name('custom.verify');
 
-Route::get('/verify', 'AuthController@verify')->name('custom.verify');
+Route::middleware('api')->post('auth/login', 'AuthController@login')->name('api.login');
+Route::middleware(['jwt.auth', 'api'])->post('auth/logout', 'AuthController@logout')->name('api.logout');
+Route::middleware(['jwt.auth', 'api'])->post('auth/refresh', 'AuthController@refresh')->name('api.refresh');
+Route::middleware(['jwt.auth', 'api'])->post('auth/me', 'AuthController@me')->name('api.me');
 
-Route::prefix('auth')->middleware('api')->post('login', 'AuthController@login')->name('api.login');
-Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('logout', 'AuthController@logout')->name('api.logout');
-Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('refresh', 'AuthController@refresh')->name('api.refresh');
-Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('me', 'AuthController@me')->name('api.me');
+Route::middleware(['jwt.auth', 'api'])->get('conversations/index', 'ConversationController@index');
+Route::middleware(['jwt.auth', 'api'])->get('conversations/show/{conversation}', 'ConversationController@show');
 
-Route::prefix('conversations')->middleware(['api', 'jwt.auth'])->get('index', 'ConversationController@index');
-Route::prefix('conversations')->middleware(['api', 'jwt.auth'])->get('/show/{conversation}', 'ConversationController@show');
-
-Route::prefix('messages')->middleware(['api', 'jwt.auth'])->post('store', 'MessageController@store');
-Route::prefix('messages')->middleware(['api', 'jwt.auth'])->delete('/destroy/{message}', 'MessageController@destroy');
+Route::middleware(['jwt.auth', 'api'])->post('messages/store', 'MessageController@store');
+Route::middleware(['jwt.auth', 'api'])->delete('messages/destroy/{message}', 'MessageController@destroy');
