90544c68d93affb47403b522c9a6e8b10a19717e
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-03-06 16:16:31 +0500

    add messages, conversations routes

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,13 @@ Route::get('/register', 'AuthController@refister')->name('custom.register');
 
 Route::get('/verify', 'AuthController@verify')->name('custom.verify');
 
-    Route::prefix('auth')->middleware('api')->post('login', 'AuthController@login')->name('api.login');
-    Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('logout', 'AuthController@logout')->name('api.logout');
-    Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('refresh', 'AuthController@refresh')->name('api.refresh');
-    Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('me', 'AuthController@me')->name('api.me');
+Route::prefix('auth')->middleware('api')->post('login', 'AuthController@login')->name('api.login');
+Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('logout', 'AuthController@logout')->name('api.logout');
+Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('refresh', 'AuthController@refresh')->name('api.refresh');
+Route::prefix('auth')->middleware(['jwt.auth', 'api'])->post('me', 'AuthController@me')->name('api.me');
+
+Route::prefix('conversations')->middleware(['api', 'jwt.auth'])->get('index', 'ConversationController@index');
+Route::prefix('conversations')->middleware(['api', 'jwt.auth'])->get('/show/{conversation}', 'ConversationController@show');
+
+Route::prefix('messages')->middleware(['api', 'jwt.auth'])->post('store', 'MessageController@store');
+Route::prefix('messages')->middleware(['api', 'jwt.auth'])->delete('/destroy/{message}', 'MessageController@destroy');
