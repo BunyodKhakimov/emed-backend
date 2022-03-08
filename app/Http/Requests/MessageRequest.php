eafbba9c20e38924bf47c4eac1b87df994fc2ae9
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-03-08 13:39:41 +0500

    add return response on failed validation

--- app/Http/Requests/MessageRequest.php
+++ app/Http/Requests/MessageRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class MessageRequest extends FormRequest
 {
@@ -28,5 +30,17 @@ class MessageRequest extends FormRequest
             'to' => 'required|numeric|exists:users,id',
             'body' => 'required|string',
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.*
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
