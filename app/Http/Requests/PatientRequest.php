430cfcf483685535358a452a625430c6b26ba608
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-04-10 17:40:10 +0500

    add patient request

---
+++ app/Http/Requests/PatientRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'gender' => ['required', 'string', 'regex:/^(?:male|female)$/'],
+            'age' => 'required|integer',
+            'height' => 'sometimes|numeric',
+            'weight' => 'sometimes|numeric',
+            'blood_pressure' => 'sometimes|numeric',
+            'temperature' => 'sometimes|numeric',
+        ];
+    }
+}
