ec3fd67969bf84b41c78bd72c33519410db2887e
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-04-10 17:40:01 +0500

    add doctor request

---
+++ app/Http/Requests/DoctorRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DoctorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'speciality' => 'required|string',
+            'info' => 'required|string',
+            'patients' => 'sometimes|integer',
+            'experience' => 'sometimes|integer',
+            'rating' => 'sometimes|numeric',
+        ];
+    }
+}
