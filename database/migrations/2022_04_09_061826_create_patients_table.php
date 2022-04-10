f595ac496a6da5f615d588c9194e590809ef6291
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-04-10 17:39:19 +0500

    create patients table

---
+++ database/migrations/2022_04_09_061826_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('gender');
+            $table->integer('age');
+            $table->float('height')->nullable()->default(NULL);
+            $table->float('weight')->nullable()->default(NULL);
+            $table->float('blood_pressure')->nullable()->default(NULL);
+            $table->float('temperature')->nullable()->default(NULL);
+            $table->timestamps();
+
+            $table->foreign('user_id')->on('users')->references('id')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+}
