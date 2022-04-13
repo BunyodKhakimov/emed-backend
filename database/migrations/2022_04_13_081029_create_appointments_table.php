6e6be4fb02d5fc19fabf446f08693f773a8bf1a7
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-04-13 13:33:58 +0500

    create appointments table

---
+++ database/migrations/2022_04_13_081029_create_appointments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppointmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->date('date');
+            $table->time('time');
+            $table->string('purpose');
+            $table->boolean('is_approved')->nullable()->default(false);
+            $table->timestamps();
+
+            $table->foreign('patient_id')->on('users')->references('id')->cascadeOnDelete();
+            $table->foreign('doctor_id')->on('users')->references('id')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+}
