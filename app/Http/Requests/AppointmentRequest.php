bfec3ed7b6209c2c7a648df08a10449aa2b31c29
BunyodKhakimov <b.khakimov@student.inha.uz>
2022-04-13 14:25:08 +0500

    add id field appointment resource

--- app/Http/Requests/AppointmentRequest.php
+++ app/Http/Requests/AppointmentRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class AppointmentRequest extends FormRequest
 {
@@ -29,5 +31,17 @@ class AppointmentRequest extends FormRequest
             'time' => 'required|date_format:H:i',
             'purpose' => 'required|string',
         ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.*
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'errors' => $validator->errors(),
+        ], 422));
     }
 }
